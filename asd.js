e3b3dd895a070264dc31aee93608e06389b084d5
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-03 18:42:42 -0300

    pusimos el ffetch

---
+++ asd.js
@@ -0,0 +1,9 @@
+export default function checkSledJump(heights) {
+  // ¡No olvides compartir tu solución en redes!
+  for(let i = 0; i < heights.length; i++) {
+    if(heights[i] < heights[i + 1]) {
+      
+    }
+  }
+  return false
+}
